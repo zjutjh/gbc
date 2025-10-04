a18328723fb9c9b80cb384374eb822803ac51f8e
XiMo-210 <2831802697@qq.com>
2025-10-05 00:32:53 +0800

    fix: correct parameter name

--- template/api.template.go
+++ template/api.template.go
@@ -50,7 +50,7 @@ func hf{$ApiStruct}(ctx *gin.Context) {
 	err := api.Init(ctx)
 	if err != nil {
 		nlog.Pick().WithContext(ctx).WithError(err).Warn("参数绑定校验错误")
-		reply.Fail(ctx, comm.CodeParamterInvalid)
+		reply.Fail(ctx, comm.CodeParameterInvalid)
 		return
 	}
 	code := api.Run(ctx)
